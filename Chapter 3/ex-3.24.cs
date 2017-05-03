0d15ee2c1f17427a0bc82c9ada71c31b9e61a05f
Pedrucius <fabiopedruci@gmail.com>
2017-05-03 20:19:13 -0300

    Chapter 3 - Exercise 3.24

---
+++ Chapter 3/ex-3.24.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+ * Exercise 3.24
+ * Answers by Fabio Pedruci
+ * Write an app that reads an integer, then determines and displays whether
+   it’s odd or even.
+*/
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please insert a number: ");
+        int num = int.Parse(Console.ReadLine());
+        if ((num % 2) == 0)
+        {
+            Console.WriteLine("This number is even");
+        }
+        else
+        {
+            Console.WriteLine("This number is odd");
+        }
+    }
+}
+
