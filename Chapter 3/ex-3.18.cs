ec25ab29cbccfbdecfc889fb9a8ca178e18786e8
Pedrucius <fabiopedruci@gmail.com>
2017-05-03 17:25:03 -0300

    Chapter 3 - Exercise 3.18

---
+++ Chapter 3/ex-3.18.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+ * Exercise 3.18
+ * Answers by Fabio Pedruci
+ * Write an app that displays a box, an oval, an arrow and
+   a diamond using asterisks (*)
+*/
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*********        ***          *             *    ");
+        Console.WriteLine("*       *      *     *       ***           * *   ");
+        Console.WriteLine("*       *     *       *     *****         *   *  ");
+        Console.WriteLine("*       *     *       *       *          *     * ");
+        Console.WriteLine("*       *     *       *       *         *       *");
+        Console.WriteLine("*       *     *       *       *          *     * ");
+        Console.WriteLine("*       *     *       *       *           *   *  ");
+        Console.WriteLine("*       *      *     *        *            * *   ");
+        Console.WriteLine("*********        ***          *             *    ");
+    }
+}
+
