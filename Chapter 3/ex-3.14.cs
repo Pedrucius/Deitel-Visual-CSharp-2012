63f6c62f60d41f12915fcb9dd26f3b99d4a8a27e
Pedrucius <fabiopedruci@gmail.com>
2017-05-03 13:36:35 -0300

    Chapter 3 - Exercise 3.14

---
+++ Chapter 3/ex-3.14.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 2 3 4");
+        Console.Write("1 ");
+        Console.Write("2 ");
+        Console.Write("3 ");
+        Console.Write("4\n");
+        Console.WriteLine("{0} {1} {2} {3}", 1, 2, 3, 4);
+    }
+}
